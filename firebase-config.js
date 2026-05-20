4440dc0d1d907c1a9981645a0605f94866fc6b6a
crsdevadmin <129576441+crsdevadmin@users.noreply.github.com>
2026-05-20 14:06:09 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+window.ZAFRAAN_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBeS-WfVLF1zIVVcZqonYpZ71yDK-XgNMQ",
+  authDomain: "zafraan-666ea.firebaseapp.com",
+  projectId: "zafraan-666ea",
+  storageBucket: "zafraan-666ea.firebasestorage.app",
+  messagingSenderId: "502276631276",
+  appId: "1:502276631276:web:c70b57be9c7a1de7fed3c9"
+};
+
+window.ZAFRAAN_ADMIN_EMAILS = [
+  "padhomjs@gmail.com"
+];
